960db675e2d77f1ee08999972215cc6d4dce3a96
MLach <mlach53@icloud.com>
2022-08-08 21:34:05 +0200

    test

---
+++ src/main/java/com/spr/systemplacereservation/SystemplacereservationApplication.java
@@ -0,0 +1,13 @@
+package com.spr.systemplacereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemplacereservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemplacereservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spr/systemplacereservation/SystemplacereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spr.systemplacereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemplacereservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
