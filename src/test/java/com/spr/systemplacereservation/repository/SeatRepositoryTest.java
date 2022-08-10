d00e0ab2e1ed1edffc043dd5a8e2665f82c323a6
MLach <mlach53@icloud.com>
2022-08-10 11:57:51 +0200

    seat repo tests

--- src/test/java/com/spr/systemplacereservation/repository/SeatRepositoryTest.java
+++ src/test/java/com/spr/systemplacereservation/repository/SeatRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.spr.systemplacereservation.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -14,26 +18,43 @@ class SeatRepositoryTest extends SystemplacereservationApplicationTests {
 
 	@Autowired
 	SeatRepository repository;
-	
+
+	@Autowired
+	OfficeBuildingRepository repo2;
+
 	OfficeBuilding officeBuilding;
-	
+
 	Seat seat;
-	
+
 	@BeforeEach
 	void setUp() {
 		officeBuilding = new OfficeBuilding();
 		officeBuilding.setName("Rybnik");
 		seat = new Seat();
 		seat.setFloor_number(1);
+		seat.setSeat_number(1);
+		seat.setOfficeBuilding(officeBuilding);
 	}
-	
-	
+
 	@Transactional
 	@Test
 	void test() {
+		// when
+		int idOf = repo2.save(officeBuilding).getId();
+		int idSe = repository.save(seat).getId();
+
+		Optional<Seat> opt = repository.findById(idSe);
+		Optional<OfficeBuilding> optOf = repo2.findById(idOf);
 		
 		
+		// then
+		assertEquals(true, opt.isPresent());
+		assertEquals(true, optOf.isPresent());
 		
+		System.out.println(opt.get());
+		System.out.println(optOf.get());
+		
+			
 	}
 
 }
