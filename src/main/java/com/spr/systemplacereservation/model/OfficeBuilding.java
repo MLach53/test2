1c107a30aa007ad8ccd461c9deb995505f7cc089
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-08-10 11:18:16 +0200

    Seat

--- src/main/java/com/spr/systemplacereservation/model/OfficeBuilding.java
+++ src/main/java/com/spr/systemplacereservation/model/OfficeBuilding.java
@@ -1,11 +1,14 @@
 package com.spr.systemplacereservation.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,15 +21,14 @@ public class OfficeBuilding {
 
     private String name;
 
+    @OneToMany(mappedBy = "officeBuilding")
+    private List<Seat> seats = new ArrayList<>();
+
     public OfficeBuilding() {
     }
 
     public int getId() {
 	return id;
-    }
-
-    public void setId(int id) {
-	this.id = id;
     }
 
     public String getName() {
@@ -37,9 +39,17 @@ public class OfficeBuilding {
 	this.name = name;
     }
 
+    public List<Seat> getSeats() {
+	return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+	this.seats = seats;
+    }
+
     @Override
     public int hashCode() {
-	return Objects.hash(id, name);
+	return Objects.hash(id, name, seats);
     }
 
     @Override
@@ -51,7 +61,7 @@ public class OfficeBuilding {
 	if (getClass() != obj.getClass())
 	    return false;
 	OfficeBuilding other = (OfficeBuilding) obj;
-	return id == other.id && Objects.equals(name, other.name);
+	return id == other.id && Objects.equals(name, other.name) && Objects.equals(seats, other.seats);
     }
 
 }
--- src/main/java/com/spr/systemplacereservation/model/Seat.java
+++ src/main/java/com/spr/systemplacereservation/model/Seat.java
@@ -1,11 +1,16 @@
 package com.spr.systemplacereservation.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Seat {
@@ -17,4 +22,60 @@ public class Seat {
     @ManyToOne
     @JoinColumn(name = "officebuilding_id")
     private OfficeBuilding officeBuilding;
+
+    private int floor_number;
+
+    @OneToMany(mappedBy = "seat")
+    private List<Reservation> reservation = new ArrayList<>();
+
+    public Seat() {
+    }
+
+    public OfficeBuilding getOfficeBuilding() {
+	return officeBuilding;
+    }
+
+    public void setOfficeBuilding(OfficeBuilding officeBuilding) {
+	this.officeBuilding = officeBuilding;
+    }
+
+    public int getFloor_number() {
+	return floor_number;
+    }
+
+    public void setFloor_number(int floor_number) {
+	this.floor_number = floor_number;
+    }
+
+    public List<Reservation> getReservation() {
+	return reservation;
+    }
+
+    public void setReservation(List<Reservation> reservation) {
+	this.reservation = reservation;
+    }
+
+    public int getId() {
+	return id;
+    }
+
+    @Override
+    public int hashCode() {
+	return Objects.hash(floor_number, id, officeBuilding, reservation);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Seat other = (Seat) obj;
+	return floor_number == other.floor_number && id == other.id
+		&& Objects.equals(officeBuilding, other.officeBuilding)
+		&& Objects.equals(reservation, other.reservation);
+    }
+
 }
