29bfa6582bbb0512e0d6d28f7a9dda436449077e
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-08-10 10:56:44 +0200

    dawaszadw

--- src/main/java/com/spr/systemplacereservation/model/OfficeBuilding.java
+++ src/main/java/com/spr/systemplacereservation/model/OfficeBuilding.java
@@ -3,47 +3,44 @@ package com.spr.systemplacereservation.model;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "OfficeBuilding")
 public class OfficeBuilding {
-    
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    
+
     private String name;
-    
-    
 
     public OfficeBuilding() {
     }
 
-    
-    
     public int getId() {
-        return id;
+	return id;
     }
 
     public void setId(int id) {
-        this.id = id;
+	this.id = id;
     }
 
     public String getName() {
-        return name;
+	return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
-
-
 
     @Override
     public int hashCode() {
 	return Objects.hash(id, name);
     }
-
-
 
     @Override
     public boolean equals(Object obj) {
@@ -56,6 +53,5 @@ public class OfficeBuilding {
 	OfficeBuilding other = (OfficeBuilding) obj;
 	return id == other.id && Objects.equals(name, other.name);
     }
-    
-    
+
 }
