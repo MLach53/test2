703ca09673bee59eceaecda6beed036ddaf8eac0
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-08-10 11:03:34 +0200

    seat

--- src/main/java/com/spr/systemplacereservation/model/Seat.java
+++ src/main/java/com/spr/systemplacereservation/model/Seat.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Seat {
@@ -11,4 +13,8 @@ public class Seat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "officebuilding_id")
+    private OfficeBuilding officeBuilding;
 }
