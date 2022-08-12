6a8306cde8bc884de54cad0f76976e9521035f62
MLach <mlach53@icloud.com>
2022-08-12 15:10:16 +0200

    testy porojobione

--- src/test/java/com/spr/systemplacereservation/repository/DeleteReservationTest.java
+++ src/test/java/com/spr/systemplacereservation/repository/DeleteReservationTest.java
@@ -9,22 +9,22 @@ import com.spr.systemplacereservation.services.ReservationService;
 
 public class DeleteReservationTest extends SystemplacereservationApplicationTests {
 
-    @Autowired
-    private SeatRepository seatRepository;
+	@Autowired
+	private SeatRepository seatRepository;
 
-    @Autowired
-    private ReservationRepository reservationRepository;
+	@Autowired
+	private ReservationRepository reservationRepository;
 
-    @Autowired
-    private SeatRepositoryDAO dao;
+	@Autowired
+	private SeatRepositoryDAO dao;
 
-    @Autowired
-    private ReservationService reservationService;
+	@Autowired
+	private ReservationService reservationService;
 
-    @Test
-    void test() {
-	reservationService.deleteReservation(Integer.valueOf(3));
-	ReservationDTO reservationDTO = new ReservationDTO();
+	@Test
+	void test() {
+		reservationService.deleteReservation(Integer.valueOf(3));
+		ReservationDTO reservationDTO = new ReservationDTO();
 
-    }
+	}
 }
