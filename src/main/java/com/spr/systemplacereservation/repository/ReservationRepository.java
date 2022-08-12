5df06fcb702268a3791c3ae37c52de761f403cf2
MLach <mlach53@icloud.com>
2022-08-12 14:31:36 +0200

    repos fix

--- src/main/java/com/spr/systemplacereservation/repository/ReservationRepository.java
+++ src/main/java/com/spr/systemplacereservation/repository/ReservationRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spr.systemplacereservation.entity.Reservation;
@@ -13,10 +14,13 @@ import com.spr.systemplacereservation.entity.Reservation;
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
 
-	@Query(value = "select r from R")
-	Optional<Reservation> findFirstByDateAndPersonIdAndOfficeBuildingId(LocalDate date, Integer personId,
-			Integer officeBuildingId);
+	// @Query(value = "select r, s from Reservation inner join Seat s on r.seatId =
+	// s")
 
-	List<Reservation> findAllbyDate(LocalDate date);
+	@Query(value = "select r,s from Reservation r left outer join Seat s on s.id = r.seat.id where s.officeBuilding.id = :officeBuildingId AND r.personId= :personId AND r.date=:date")
+	Optional<Reservation> findFirstByDateAndPersonIdAndOfficeBuildingId(@Param(value = "date") LocalDate date,
+			@Param(value = "personId") Integer personId, @Param(value = "officeBuildingId") Integer officeBuildingId);
+
+	List<Reservation> findByDate(LocalDate date);
 
 }
