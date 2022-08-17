fc252701a8f7278cf84f7db01a75741ebb16e671
MLach <mlach53@icloud.com>
2022-08-17 11:55:24 +0200

    reservation sservice tests

--- src/test/java/com/spr/systemplacereservation/services/ReservationServiceTest.java
+++ src/test/java/com/spr/systemplacereservation/services/ReservationServiceTest.java
@@ -1,21 +1,29 @@
 package com.spr.systemplacereservation.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.spr.systemplacereservation.SystemplacereservationApplicationTests;
 import com.spr.systemplacereservation.entity.OfficeBuilding;
 import com.spr.systemplacereservation.entity.Reservation;
 import com.spr.systemplacereservation.entity.Seat;
+import com.spr.systemplacereservation.entity.dto.ReservationDTO;
+import com.spr.systemplacereservation.entity.dto.ReservationWithoutDateDTO;
+import com.spr.systemplacereservation.entity.dto.UpdateReservationDTO;
+import com.spr.systemplacereservation.exceptions.NotAvailableException;
+import com.spr.systemplacereservation.exceptions.UserAlreadyReservedChairException;
 import com.spr.systemplacereservation.repository.OfficeBuildingRepository;
 import com.spr.systemplacereservation.repository.ReservationRepository;
 import com.spr.systemplacereservation.repository.SeatRepository;
@@ -34,12 +42,20 @@ class ReservationServiceTest extends SystemplacereservationApplicationTests {
 	private Seat seatThree;
 	private Seat seatFour;
 
+	private ReservationDTO dtoOne;
+	private ReservationDTO dtoTwo;
+	private ReservationDTO dtoThree;
+	private ReservationDTO dtoFour;
+
 	@Autowired
 	private OfficeBuildingRepository repository;
 	@Autowired
 	private SeatRepository seatRepository;
 	@Autowired
 	private ReservationRepository reservationRepository;
+
+	@Autowired
+	private ReservationService service;
 
 	@BeforeEach
 	void setUp() {
@@ -57,107 +73,215 @@ class ReservationServiceTest extends SystemplacereservationApplicationTests {
 		seatOne.setFloorNumber(0);
 		seatOne.setOfficeBuilding(building);
 		seatOne.setSeatNumber("A");
+		seatOne.setReservationeligible(true);
 		seatRepository.save(seatOne);
 
 		seatTwo = new Seat();
 		seatTwo.setFloorNumber(0);
 		seatTwo.setOfficeBuilding(building);
 		seatTwo.setSeatNumber("B");
+		seatTwo.setReservationeligible(true);
 		seatRepository.save(seatTwo);
 
 		seatThree = new Seat();
 		seatThree.setFloorNumber(0);
 		seatThree.setOfficeBuilding(buildingTwo);
 		seatThree.setSeatNumber("A");
+		seatThree.setReservationeligible(true);
 		seatRepository.save(seatThree);
 
 		seatFour = new Seat();
 		seatFour.setFloorNumber(0);
 		seatFour.setOfficeBuilding(buildingTwo);
 		seatFour.setSeatNumber("B");
+		seatFour.setReservationeligible(false);
+		seatFour.setReDescription("seat is under maintenance");
 		seatRepository.save(seatFour);
 
-		one = new Reservation();
-		two = new Reservation();
-		three = new Reservation();
+		dtoOne = new ReservationDTO();
+		dtoOne.setDate(LocalDate.of(2022, 8, 14));
+		dtoOne.setFloorNumber(seatOne.getFloorNumber());
+		dtoOne.setSeatNumber(seatOne.getSeatNumber());
+		dtoOne.setOfficeBuildingId(seatOne.getOfficeBuilding().getId());
+		dtoOne.setPersonId(5);
 
-		one.setPersonId(1);
-		one.setDate(LocalDate.of(2022, 8, 14));
-		one.setSeat(seatOne);
+		dtoTwo = new ReservationDTO();
+		dtoTwo.setDate(LocalDate.of(2022, 8, 15));
+		dtoTwo.setFloorNumber(seatFour.getFloorNumber());
+		dtoTwo.setSeatNumber(seatFour.getSeatNumber());
+		dtoTwo.setOfficeBuildingId(seatFour.getOfficeBuilding().getId());
+		dtoTwo.setPersonId(5);
 
-		two.setPersonId(2);
-		two.setDate(LocalDate.of(2022, 8, 14));
-		two.setSeat(seatTwo);
+		dtoThree = new ReservationDTO();
+		dtoThree.setDate(LocalDate.of(2022, 8, 16));
+		dtoThree.setFloorNumber(seatThree.getFloorNumber());
+		dtoThree.setSeatNumber(seatThree.getSeatNumber());
+		dtoThree.setOfficeBuildingId(seatThree.getOfficeBuilding().getId());
+		dtoThree.setPersonId(5);
 
-		three.setDate(LocalDate.of(2022, 8, 15));
-		three.setPersonId(1);
-		three.setSeat(seatThree);
-
-		reservationRepository.save(one);
-		reservationRepository.save(two);
-		reservationRepository.save(three);
-
-	}
-
-	// @Test
-	void testFindFirstByDateAndPersonIdAndOfficeBuildingIdPresent() {
-
-		// when
-		Optional<Reservation> optional = reservationRepository
-				.findFirstByDateAndPersonIdAndOfficeBuildingId(LocalDate.of(2022, 8, 14), 2, building.getId());
-
-		// then
-		assertEquals(true, optional.isPresent());
-
-	}
-
-	// @Test
-	void testFindFirstByDateAndPersonIdAndOfficeBuildingIdEmpty() {
-
-		// when
-		Optional<Reservation> optional = reservationRepository
-				.findFirstByDateAndPersonIdAndOfficeBuildingId(LocalDate.of(2022, 8, 15), 2, building.getId());
-
-		// then
-		assertEquals(true, optional.isEmpty());
-
-	}
-
-	// @Test
-	void testFindByDate() {
-
-		// when
-		List<Reservation> value = reservationRepository.findByDate(LocalDate.of(2022, 8, 14));
-
-		// then
-		assertFalse(value.isEmpty());
-
-	}
-
-	// @Test
-	void testFindByDateEmpty() {
-
-		// when
-		List<Reservation> value = reservationRepository.findByDate(LocalDate.of(2022, 8, 13));
-
-		// then
-		assertTrue(value.isEmpty());
 	}
 
 	@Test
-	void testFindByDateBetween() {
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("ReservationRepositoryTest2.testFindByDateBetween()");
+	void testMakeReservation() {
 
-		reservationRepository.findByDateBetween(LocalDate.of(2022, 8, 13), LocalDate.of(2022, 8, 14))
-				.forEach(System.out::println);
+		// when
+		Reservation reservation = service.makeReservation(dtoOne);
 
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("-------------------------------------------------------------");
+		// then
+		Assertions.assertNotNull(reservation);
+	}
+
+	@Test
+	void testMakeReservationChairNotAvaliable() {
+
+		// then
+		Assertions.assertThrows(NotAvailableException.class, () -> {
+
+			// when
+			service.makeReservation(dtoTwo);
+
+		});
+
+	}
+
+	@Test
+	void testMakeReservationUserAlreadyRegistered() {
+
+		// given
+		Reservation reservation = service.makeReservation(dtoOne);
+		dtoOne.setSeatNumber(seatTwo.getSeatNumber());
+
+		// then
+		Assertions.assertThrows(UserAlreadyReservedChairException.class, () -> {
+
+			// when
+			service.makeReservation(dtoOne);
+
+		});
+
+	}
+
+	@Test
+	void testMakeReservationNoSuchElementException() {
+
+		// given
+		Reservation reservation = service.makeReservation(dtoOne);
+		dtoOne.setSeatNumber(seatTwo.getSeatNumber());
+
+		ReservationDTO dto = new ReservationDTO();
+		dto.setFloorNumber(0);
+		dto.setOfficeBuildingId(0);
+		dto.setPersonId(0);
+		dto.setSeatNumber("KK");
+		dto.setDate(LocalDate.of(2022, 8, 14));
+
+		// then
+		Assertions.assertThrows(NoSuchElementException.class, () -> {
+
+			// when
+			service.makeReservation(dto);
+
+		});
+
+	}
+
+	@Test
+	void testMakeReservationDataIntegrityViolationException() {
+
+		// given
+		Reservation reservation = service.makeReservation(dtoOne);
+		dtoOne.setPersonId(3);
+
+		// then
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+
+			// when
+			service.makeReservation(dtoOne);
+
+		});
+
+	}
+
+	@Test
+	void testDeleteReservation() {
+
+		// given
+		Reservation reservation = service.makeReservation(dtoOne);
+
+		// then
+		Assertions.assertDoesNotThrow(() -> {
+
+			// when
+			service.deleteReservation(reservation.getId());
+
+		});
+
+	}
+
+	@Test
+	void testDeleteReservationEmptyResultDateAccess() {
+
+		// then
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+
+			// when
+			service.deleteReservation(-1);
+
+		});
+
+	}
+
+	@Test
+	void testGetReservationsAtGivenDateAndSpan() {
+
+		// given
+		service.makeReservation(dtoOne);
+		service.makeReservation(dtoThree);
+
+		// dtoThree.setOfficeBuildingId(seatThree.getOfficeBuilding().getId());
+		// dtoThree.setSeatNumber(seatThree.getSeatNumber());
+		// dtoThree.setFloorNumber(seatThree.getFloorNumber());
+		dtoThree.setDate(LocalDate.of(2022, 8, 14));
+		dtoThree.setPersonId(4);
+
+		service.makeReservation(dtoThree);
+
+		// when
+		List<ReservationDTO> list1408 = service.getReservationsAtGivenDate(LocalDate.of(2022, 8, 14));
+		List<ReservationDTO> list1508 = service.getReservationsAtGivenDate(LocalDate.of(2022, 8, 15));
+		List<ReservationDTO> list1608 = service.getReservationsAtGivenDate(LocalDate.of(2022, 8, 16));
+
+		Map<LocalDate, List<ReservationWithoutDateDTO>> betweenDates = service
+				.getReserervationsAtGivenTimeSpan(LocalDate.of(2022, 8, 14), LocalDate.of(2022, 8, 16));
+
+		// then
+		assertEquals(2, list1408.size());
+		assertEquals(1, list1608.size());
+		assertEquals(0, list1508.size());
+		assertEquals(2, betweenDates.size());
+
+		assertEquals(list1408.size(), betweenDates.get(LocalDate.of(2022, 8, 14)).size());
+		assertEquals(list1608.size(), betweenDates.get(LocalDate.of(2022, 8, 16)).size());
+
+	}
+
+	@Test
+	void testUpdateReservation() {
+
+		Reservation reservation = service.makeReservation(dtoThree);
+
+		UpdateReservationDTO dto = new UpdateReservationDTO();
+		dto.setDate(LocalDate.of(2022, 8, 13));
+		dto.setFloorNumber(seatOne.getFloorNumber());
+		dto.setSeatNumber(seatOne.getSeatNumber());
+		dto.setPersonId(reservation.getPersonId());
+		dto.setId(reservation.getId());
+		dto.setOfficeBuildingId(seatOne.getOfficeBuilding().getId());
+
+		Reservation updatedReservation = service.updateReservation(dto);
+
+		assertNotEquals(reservation, updatedReservation);
+
 	}
 
 }
