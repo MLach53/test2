8af3b4eb3960b5a4acc986667cd376f151e77779
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-08-23 12:58:27 +0200

    DTO

--- src/main/java/com/spr/systemplacereservation/entity/dto/ReservationWithoutDateDTO.java
+++ src/main/java/com/spr/systemplacereservation/entity/dto/ReservationWithoutDateDTO.java
@@ -1,5 +1,7 @@
 package com.spr.systemplacereservation.entity.dto;
 
+import java.time.LocalDate;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -8,85 +10,96 @@ import com.spr.systemplacereservation.entity.Reservation;
 
 public class ReservationWithoutDateDTO {
 
-	@NotNull
-	@Min(1)
-	private Integer officeBuildingId;
+    @NotNull
+    @Min(1)
+    private Integer officeBuildingId;
 
-	@NotNull
-	private Integer floorNumber;
+    @NotNull
+    private Integer floorNumber;
 
-	@NotEmpty
-	private String seatNumber;
+    @NotEmpty
+    private String seatNumber;
 
-	@NotNull
-	private Integer personId;
+    @NotNull
+    private Integer personId;
 
-	private Integer id;
+    private Integer id;
 
-	public static ReservationWithoutDateDTO convertToDto(Reservation reservation) {
-		return new ReservationWithoutDateDTO(reservation.getSeat().getOfficeBuilding().getId(),
-				reservation.getSeat().getFloorNumber(), reservation.getSeat().getSeatNumber(),
-				reservation.getPersonId(), reservation.getId());
-	}
+    private LocalDate createdOn;
 
-	public ReservationWithoutDateDTO(@NotNull @Min(1) Integer officeBuildingId, @NotNull Integer floorNumber,
-			@NotEmpty String seatNumber, @NotNull Integer personId, Integer id) {
-		super();
-		this.officeBuildingId = officeBuildingId;
-		this.floorNumber = floorNumber;
-		this.seatNumber = seatNumber;
-		this.personId = personId;
-		this.id = id;
-	}
+    public static ReservationWithoutDateDTO convertToDto(Reservation reservation) {
+	return new ReservationWithoutDateDTO(reservation.getSeat().getOfficeBuilding().getId(),
+		reservation.getSeat().getFloorNumber(), reservation.getSeat().getSeatNumber(),
+		reservation.getPersonId(), reservation.getId(), reservation.getCreationDate());
+    }
 
-	public ReservationWithoutDateDTO() {
-		super();
-	}
+    public ReservationWithoutDateDTO(@NotNull @Min(1) Integer officeBuildingId, @NotNull Integer floorNumber,
+	    @NotEmpty String seatNumber, @NotNull Integer personId, Integer id, LocalDate createdOn) {
+	super();
+	this.officeBuildingId = officeBuildingId;
+	this.floorNumber = floorNumber;
+	this.seatNumber = seatNumber;
+	this.personId = personId;
+	this.id = id;
+	this.createdOn = createdOn;
+    }
 
-	public Integer getOfficeBuildingId() {
-		return officeBuildingId;
-	}
+    public ReservationWithoutDateDTO() {
+	super();
+    }
 
-	public void setOfficeBuildingId(Integer officeBuildingId) {
-		this.officeBuildingId = officeBuildingId;
-	}
+    public Integer getOfficeBuildingId() {
+	return officeBuildingId;
+    }
 
-	public Integer getFloorNumber() {
-		return floorNumber;
-	}
+    public void setOfficeBuildingId(Integer officeBuildingId) {
+	this.officeBuildingId = officeBuildingId;
+    }
 
-	public void setFloorNumber(Integer floorNumber) {
-		this.floorNumber = floorNumber;
-	}
+    public Integer getFloorNumber() {
+	return floorNumber;
+    }
 
-	public String getSeatNumber() {
-		return seatNumber;
-	}
+    public void setFloorNumber(Integer floorNumber) {
+	this.floorNumber = floorNumber;
+    }
 
-	public void setSeatNumber(String seatNumber) {
-		this.seatNumber = seatNumber;
-	}
+    public String getSeatNumber() {
+	return seatNumber;
+    }
 
-	public Integer getPersonId() {
-		return personId;
-	}
+    public void setSeatNumber(String seatNumber) {
+	this.seatNumber = seatNumber;
+    }
 
-	public void setPersonId(Integer personId) {
-		this.personId = personId;
-	}
+    public Integer getPersonId() {
+	return personId;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public void setPersonId(Integer personId) {
+	this.personId = personId;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	@Override
-	public String toString() {
-		return "ReservationWithoutDateDTO [officeBuildingId=" + officeBuildingId + ", floorNumber=" + floorNumber
-				+ ", seatNumber=" + seatNumber + ", personId=" + personId + ", id=" + id + "]";
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
+
+    public LocalDate getCreatedOn() {
+	return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+	this.createdOn = createdOn;
+    }
+
+    @Override
+    public String toString() {
+	return "ReservationWithoutDateDTO [officeBuildingId=" + officeBuildingId + ", floorNumber=" + floorNumber
+		+ ", seatNumber=" + seatNumber + ", personId=" + personId + ", id=" + id + "]";
+    }
 
 }
