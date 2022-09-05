0aceb11792afb81e1a65d89ef95617f3ef3139bf
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-09-05 22:13:57 +0200

    comit

--- src/main/java/com/spr/systemplacereservation/SystemplacereservationApplication.java
+++ src/main/java/com/spr/systemplacereservation/SystemplacereservationApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SystemplacereservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemplacereservationApplication.class, args);
-
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(SystemplacereservationApplication.class, args);
+	int x = 2;
+    }
 
 }
