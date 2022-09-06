44f13f1705386cb57c1eef0000a921487c07f09a
JanGorczynski <Jan.Aleksander.Gorczynski@gmail.com>
2022-09-06 19:03:40 +0200

    dwaszsds

--- src/main/java/com/spr/systemplacereservation/entity/Reservation.java
+++ src/main/java/com/spr/systemplacereservation/entity/Reservation.java
@@ -3,105 +3,88 @@ package com.spr.systemplacereservation.entity;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import org.hibernate.annotations.CreationTimestamp;
-
-@Entity
-@Table(name = "RESERVATION", uniqueConstraints = {
-		@UniqueConstraint(name = "UC_RESERVATION", columnNames = { "seat_id", "date" }) })
+@Document(collection = "RESERVATION")
 public class Reservation {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Integer id;
+    @Id
+    private Integer id;
 
-	@Column(name = "person_id")
-	private Integer personId;
+    private Integer personId;
 
-	@ManyToOne
-	@JoinColumn(name = "seat_id")
-	private Seat seat;
+    @DBRef
+    private Seat seat;
 
-	@Column(name = "date")
-	private LocalDate date;
+    private LocalDate date;
 
-	@Column(name = "created_on")
-	@CreationTimestamp
-	private LocalDate creationOn;
+    private LocalDate creationOn;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-	public Integer getPersonId() {
-		return personId;
-	}
+    public Integer getPersonId() {
+	return personId;
+    }
 
-	public void setPersonId(Integer personId) {
-		this.personId = personId;
-	}
+    public void setPersonId(Integer personId) {
+	this.personId = personId;
+    }
 
-	public Seat getSeat() {
-		return seat;
-	}
+    public Seat getSeat() {
+	return seat;
+    }
 
-	public void setSeat(Seat seat) {
-		this.seat = seat;
-	}
+    public void setSeat(Seat seat) {
+	this.seat = seat;
+    }
 
-	public LocalDate getDate() {
-		return date;
-	}
+    public LocalDate getDate() {
+	return date;
+    }
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+    public void setDate(LocalDate date) {
+	this.date = date;
+    }
 
-	public LocalDate getCreationDate() {
-		return creationOn;
-	}
+    public LocalDate getCreationDate() {
+	return creationOn;
+    }
 
-	public void setCreationDate(LocalDate creationDate) {
-		this.creationOn = creationDate;
-	}
+    public void setCreationDate(LocalDate creationDate) {
+	this.creationOn = creationDate;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(creationOn, date, id, personId, seat);
-	}
+    @Override
+    public int hashCode() {
+	return Objects.hash(creationOn, date, id, personId, seat);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Reservation other = (Reservation) obj;
-		return Objects.equals(creationOn, other.creationOn) && Objects.equals(date, other.date)
-				&& Objects.equals(id, other.id) && Objects.equals(personId, other.personId)
-				&& Objects.equals(seat, other.seat);
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Reservation other = (Reservation) obj;
+	return Objects.equals(creationOn, other.creationOn) && Objects.equals(date, other.date)
+		&& Objects.equals(id, other.id) && Objects.equals(personId, other.personId)
+		&& Objects.equals(seat, other.seat);
+    }
 
-	@Override
-	public String toString() {
-		return "Reservation [id=" + id + ", creationDate=" + creationOn + ", personId=" + personId + ", seat=" + seat
-				+ ", date=" + date + "]";
-	}
+    @Override
+    public String toString() {
+	return "Reservation [id=" + id + ", creationDate=" + creationOn + ", personId=" + personId + ", seat=" + seat
+		+ ", date=" + date + "]";
+    }
 
 }
