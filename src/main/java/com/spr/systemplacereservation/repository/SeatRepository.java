0b072d37ce55b29084e3dfca31a56cefa26ba561
MLach <mlach53@icloud.com>
2022-09-06 19:58:29 +0200

    mongo_2

--- src/main/java/com/spr/systemplacereservation/repository/SeatRepository.java
+++ src/main/java/com/spr/systemplacereservation/repository/SeatRepository.java
@@ -3,6 +3,8 @@ package com.spr.systemplacereservation.repository;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spr.systemplacereservation.entity.Seat;
@@ -10,6 +12,7 @@ import com.spr.systemplacereservation.entity.Seat;
 @Repository
 public interface SeatRepository extends MongoRepository<Seat, String> {
 
-	Optional<Seat> findByOfficeBuildingIdAndSeatNumberAndFloorNumber(String officeBuildingId, String seatNumber,
-			Integer floorNumber);
+	@Query(value = "select s,o from Seat s left outer join OfficeBuilding o on s.officeBuilding.id = o.id where o.id = :officeBuildingId AND s.seatNumber= :seatNumber AND s.floorNumber=:floorNumber")
+	Optional<Seat> findByOfficeBuildingIdAndSeatNumberAndFloorNumber(@Param("officeBuildingId") String officeBuildingId,
+			@Param("seatNumber") String seatNumber, @Param("floorNumber") Integer floorNumber);
 }
